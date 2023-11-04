4b79a2ec467268a387aea34fc792cf52033c36f0
rewpt <zpermack@gmail.com>
2023-11-03 23:39:19 -0400

    refactor to es module

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-const express = require('express');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { Server } from 'socket.io';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
+
 app.use(express.static(__dirname + '/public'));
+
 const expressServer = app.listen(9000);
-const socketio = require('socket.io');
-const io = socketio(expressServer);
+
+const io = new Server(expressServer);
+
+export { app, io };
