95d122d77fc66224321606e85a5946a098c62a6c
rewpt <zpermack@gmail.com>
2023-11-03 23:20:20 -0400

    html and index file

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+const expressServer = app.listen(9000);
+const socketio = require('socket.io');
+const io = socketio(expressServer);
